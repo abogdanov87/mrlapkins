7402096e5d8a9a798a454fc89cb8104741c2cd0c
aybogdanov <aybogdanov@yandex-team.ru>
2021-04-24 23:02:47 +0300

    upd ui

--- apps/catalogs/api/v1/filters.py
+++ apps/catalogs/api/v1/filters.py
@@ -11,6 +11,11 @@ CharField.register_lookup(Lower)
 
 
 class BreedFilter(filters.FilterSet):
+    title = filters.CharFilter(field_name='title', method='filter_title')
+
     class Meta:
         model = Breed
-        fields = ('active',)
+        fields = ('active', 'pet_type', 'title',)
+
+    def filter_title(self, queryset, title, value):
+        return queryset.filter(title__icontains=value)
